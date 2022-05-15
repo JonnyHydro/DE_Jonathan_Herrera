8c4d2560269baee8af55a088806f241a0e6774dd
Jonathan <jonysteel99@gmail.com>
2022-05-15 11:36:17 -0500

    Inheritance and Encapsulation

---
+++ 1.-OPP/1.4 Tasks/1.4.2 Inheritance and Encapsulation/AudioLibrary/VehicleSounds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioLibrary
+{
+    public class VehicleSounds
+    {
+        public void vehicleSounds()
+        {
+            Console.WriteLine("I'm a vehicle!");
+        }
+    }
+}
---
+++ 1.-OPP/1.4 Tasks/1.4.2 Inheritance and Encapsulation/VehicleSounds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace VehicleSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 10)
+            {
+                Vehicle vehicle;
+                Console.WriteLine("Type a vehicle: ");
+                string opc = Console.ReadLine().ToLower();
+
+                switch (opc)
+                {
+                    case "car":
+                        vehicle = new Car();
+                        vehicle.VehicleSounds();
+                        break;
+                    case "train":
+                        vehicle = new Train();
+                        vehicle.VehicleSounds();
+                        break;
+                    case "truck":
+                        vehicle = new Truck();
+                        vehicle.VehicleSounds();
+                        break;
+                    default:
+                        Console.WriteLine("Vehicle not found");
+                        System.Threading.Thread.Sleep(2000);
+                        break;
+                }   
+                i++;
+            }
+            Console.WriteLine("End of Program");
+            Console.ReadKey();
+        }
+    }
+}
